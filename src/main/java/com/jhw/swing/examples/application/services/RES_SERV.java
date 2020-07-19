e4d128d7d1147529dc36d67a723129b74e5bf999
Unknown <unknown@example.com>
2020-07-19 17:29:15 -0500

    ResourceService por bundle

--- src/main/java/com/jhw/swing/examples/application/services/RES_SERV.java
+++ src/main/java/com/jhw/swing/examples/application/services/RES_SERV.java
@@ -1,7 +1,10 @@
 package com.jhw.swing.examples.application.services;
 
 import com.clean.core.domain.services.Resource;
+import com.clean.core.domain.services.ResourceBundleUtils;
 import com.clean.core.domain.services.ResourceService;
+import com.clean.core.domain.services.DefaultResourceBundleService;
+import java.net.MalformedURLException;
 
 /**
  *
@@ -9,18 +12,23 @@ import com.clean.core.domain.services.ResourceService;
  */
 public class RES_SERV implements ResourceService {
 
-    public static RES_SERV init() {
+    private final DefaultResourceBundleService resourceService;
+
+    public static RES_SERV init() throws MalformedURLException {
         RES_SERV res = new RES_SERV();
         Resource.registerResourceService(res);
         return res;
     }
 
-    private RES_SERV() {
+    private RES_SERV() throws MalformedURLException {
+        resourceService = new DefaultResourceBundleService(
+                ResourceBundleUtils.fromExternalFile("msg",
+                        ResourceBundleUtils.SPANISH));
     }
 
     @Override
     public String getString(String string) {
-        return "123";
+        return resourceService.getString(string);
     }
 
     @Override
