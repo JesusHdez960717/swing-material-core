8507e76f29f3aa811f4a8eddef659dc27a164447
Unknown <unknown@example.com>
2020-08-31 15:27:31 -0500

    *_MaterialFileChooser

--- src/main/java/com/jhw/swing/material/components/filechooser/_MaterialFileChooser.java
+++ src/main/java/com/jhw/swing/material/components/filechooser/_MaterialFileChooser.java
@@ -1,50 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.jhw.swing.material.components.filechooser;
 
+import com.jhw.personalization.core.domain.Personalization;
+import com.jhw.personalization.services.PersonalizationHandler;
+import com.jhw.swing.material.components.button._MaterialButton;
+import com.jhw.swing.material.standards.MaterialColors;
+import com.jhw.swing.material.standards.MaterialIcons;
 import com.jhw.swing.util.interfaces.BindableComponent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import javax.swing.JFileChooser;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public class _MaterialFileChooser extends JFileChooser implements BindableComponent<File[]> {
+public class _MaterialFileChooser extends _MaterialButton implements BindableComponent<List<File>> {
+
+    private FileChooser fc;
+    private static File lastFile = new File("");
+
+    private final Consumer<List<File>> consumer = new Consumer<List<File>>() {
+        @Override
+        public void accept(List<File> t) {
+            mergeFiles(t);
+        }
+    };
+    private final List<File> selectedFiles = new ArrayList<>();
+
+    private String upload = "Upload";
 
     public _MaterialFileChooser() {
-        super(new File(""));
-        configure();
+        initComponents();
+        addListeners();
+        this.setTransferHandler(new FileDropHandler(consumer));
     }
 
-    public _MaterialFileChooser(File currentDirectory) {
-        super(currentDirectory);
-        configure();
+    private void initComponents() {
+        this.setBackground(PersonalizationHandler.getColor(Personalization.KEY_COLOR_BACKGROUND_PANEL));
+        this.setBorderColor(MaterialColors.BROWN_700);
+        this.setBorderThickness(2);
+        this.setIcon(MaterialIcons.FILE_UPLOAD);
+        this.setText(upload);
     }
 
-    public _MaterialFileChooser(String currentDirectoryPath) {
-        super(currentDirectoryPath);
-        configure();
+    private void addListeners() {
+        addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                onButtonAbrirActionPerformed();
+            }
+        });
     }
 
-    public void addConfirmListener(ActionListener action) {
-        int returnVal = this.showOpenDialog(null);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            action.actionPerformed(null);
+    private void onButtonAbrirActionPerformed() {
+        fc = new FileChooser(lastFile);
+        fc.addConfirmListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mergeFiles(Arrays.asList(fc.getSelectedFiles()));
+            }
+        });
+    }
+
+    public String getUpload() {
+        return upload;
+    }
+
+    public void setUpload(String upload) {
+        this.upload = upload;
+    }
+
+    public void clear() {
+        if (fc != null) {
+            selectedFiles.clear();
+            fc.setSelectedFiles(null);
         }
     }
 
-    private void configure() {
-        this.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        this.setMultiSelectionEnabled(true);
+    public void setSelectedFiles(List<File> files) {
+        clear();
+        selectedFiles.addAll(files);
+    }
+
+    public List<File> getSelectedFiles() {
+        return selectedFiles;
+    }
+
+    private void mergeFiles(List<File> newSelection) {
+        for (File file : newSelection) {
+            if (!selectedFiles.contains(file)) {
+                selectedFiles.add(file);
+            }
+        }
+        if (!getSelectedFiles().isEmpty()) {
+            lastFile = getSelectedFiles().get(0).getParentFile();
+            setText(upload + " (" + getSelectedFiles().size() + ")");
+        }
     }
 
     @Override
-    public File[] getObject() {
+    public List<File> getObject() {
         return getSelectedFiles();
     }
 
     @Override
-    public void setObject(File[] object) {
+    public void setObject(List<File> object) {
         setSelectedFiles(object);
     }
+
 }
