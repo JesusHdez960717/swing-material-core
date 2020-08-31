8bba8add932457e0c6519536550101d026fcac22
Unknown <unknown@example.com>
2020-08-31 15:53:07 -0500

    MISC

--- src/main/java/com/jhw/swing/material/components/filechooser/_MaterialFileChooserIcon.java
+++ src/main/java/com/jhw/swing/material/components/filechooser/_MaterialFileChooserIcon.java
@@ -43,9 +43,13 @@ public class _MaterialFileChooserIcon extends _PanelTransparent implements Binda
 
         buttonClear = new _MaterialButtonIconTransparent();
         buttonClear.setIcon(MaterialIcons.CLEAR);
+        
+        int h = (int) this.fileChooser.getPreferredSize().getHeight();
+        int w = (int) (h * _MaterialTextFieldIcon.ICON_WIDTH_REDUCTION * 0.75);
+        buttonClear.setPreferredSize(new Dimension(w, h));
 
         this.setLayout(new BorderLayout());
-        this.add(fileChooser, BorderLayout.CENTER);
+        this.add(fileChooser);
         this.add(buttonClear, BorderLayout.EAST);
     }
 
