31445e6d91c7c79f865febfc98219e85817a0930
Unknown <unknown@example.com>
2020-08-31 23:59:30 -0500

    Money por Formated

--- src/main/java/com/jhw/swing/material/components/textfield/validated/_MaterialTextFieldMoneyIcon.java
+++ src/main/java/com/jhw/swing/material/components/textfield/validated/_MaterialTextFieldMoneyIcon.java
@@ -5,6 +5,7 @@
  */
 package com.jhw.swing.material.components.textfield.validated;
 
+import com.jhw.swing.material.components.textfield._MaterialFormatedTextFieldIcon;
 import com.jhw.swing.material.components.textfield._MaterialTextFieldIcon;
 import com.jhw.swing.material.standards.MaterialIcons;
 import java.math.BigDecimal;
@@ -13,7 +14,7 @@ import java.math.BigDecimal;
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public class _MaterialTextFieldMoneyIcon extends _MaterialTextFieldIcon<BigDecimal> {
+public class _MaterialTextFieldMoneyIcon extends _MaterialFormatedTextFieldIcon<BigDecimal> {
 
     public _MaterialTextFieldMoneyIcon() {
         this(false);
