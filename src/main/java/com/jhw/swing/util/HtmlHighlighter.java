2792a443827ec99347f020ae23ba0e00ac70d1bf
Unknown <unknown@example.com>
2020-09-03 22:03:09 -0500

    HtmlHighlighter: *highlightText. Se rige por las animaciones del personalization

--- src/main/java/com/jhw/swing/util/HtmlHighlighter.java
+++ src/main/java/com/jhw/swing/util/HtmlHighlighter.java
@@ -1,17 +1,22 @@
 package com.jhw.swing.util;
 
+import com.jhw.personalization.core.domain.Personalization;
+import com.jhw.personalization.services.PersonalizationHandler;
 import java.util.regex.Pattern;
 
 public class HtmlHighlighter {
 
-    private static final String HighLightTemplate = "<span style='background:yellow;'>$1</span>";
+    private static final String HIGH_LIGHT_TEMPLATE = "<span style='background:yellow;'>$1</span>";
 
     public static String highlightText(String text, String textToHighlight) {
+        if (!PersonalizationHandler.getBoolean(Personalization.KEY_USE_ANIMATIONS)) {
+            return text;
+        }
         if (textToHighlight.length() == 0) {
             return text;
         }
         try {
-            text = text.replaceAll("(?i)(" + Pattern.quote(textToHighlight) + ")", HighLightTemplate);
+            text = text.replaceAll("(?i)(" + Pattern.quote(textToHighlight) + ")", HIGH_LIGHT_TEMPLATE);
         } catch (Exception e) {
             return text;
         }
