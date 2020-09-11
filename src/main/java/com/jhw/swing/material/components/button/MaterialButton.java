c10c1a136ca227fd76505d48741f0348fdc1d588
Unknown <unknown@example.com>
2020-09-11 00:09:00 -0500

    +Material Wrappers button & combo

---
+++ src/main/java/com/jhw/swing/material/components/button/MaterialButton.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.swing.material.components.button;
+
+import com.jhw.swing.material.effects.ColorFadeInto;
+import com.jhw.swing.material.effects.ElevationEffect;
+import com.jhw.swing.material.effects.MaterialLineBorder;
+import com.jhw.swing.util.MouseAdapterInfo;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public abstract class MaterialButton extends MaterialButtonIcon implements ColorFadeInto, MouseAdapterInfo, ElevationEffect, MaterialLineBorder {
+
+}
