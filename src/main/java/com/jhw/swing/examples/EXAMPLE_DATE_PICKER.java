da62516b7049ba4841bef8f3b0fd7ffd04dbe8a9
Unknown <unknown@example.com>
2020-09-11 14:24:58 -0500

    +Examples date picker & File chooser

---
+++ src/main/java/com/jhw/swing/examples/EXAMPLE_DATE_PICKER.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.swing.examples;
+
+//import com.jhw.swing.material.components.combobox.combobox_editable._MaterialComboBoxFiltrable;
+import com.jhw.swing.material.components.container.layout.VerticalLayoutContainer;
+import com.jhw.swing.material.components.datepicker.MaterialDatePickersFactory;
+import com.jhw.swing.material.standards.MaterialColors;
+import com.jhw.swing.ui.MaterialLookAndFeel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class EXAMPLE_DATE_PICKER extends javax.swing.JFrame {
+
+    /**
+     * Creates new form NewJFrame
+     */
+    public EXAMPLE_DATE_PICKER() {
+        initComponents();
+
+        jPanel1.setLayout(new BorderLayout());
+        jPanel1.setBackground(MaterialColors.RED_800);
+
+        VerticalLayoutContainer.builder vlc = VerticalLayoutContainer.builder();
+
+        vlc.add(MaterialDatePickersFactory.build(), true);
+        //vlc.add(MaterialDatePickersFactory.buildIcon(), true);
+
+        jPanel1.add(vlc.build());
+
+    }
+
+    private void initComponents() {
+        jPanel1 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        this.setContentPane(jPanel1);
+
+        pack();
+
+        this.setSize(new Dimension(500, 500));
+        //this.setExtendedState(MAXIMIZED_BOTH);
+
+        this.setLocationRelativeTo(null);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        } catch (Exception e) {
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EXAMPLE_DATE_PICKER().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
