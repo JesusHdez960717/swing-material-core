f04dd095cdbb2c3b8590e49d1ef5e2fb9cc61480
Unknown <unknown@example.com>
2020-11-02 15:19:41 -0500

    +_Month

---
+++ src/main/java/com/jhw/swing/material/components/datepicker/_Month.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.swing.material.components.datepicker;
+
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+/**
+ * Wrapper para el java.time.Month para poder mostrar el toString personalizado
+ * y no en inglés como el Enum.
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class _Month {
+
+    public static _Month from(int id) {
+        return new _Month(id);
+    }
+
+    public final Month month;
+
+    public _Month(int id) {
+        this.month = Month.of(id);
+    }
+
+    @Override
+    public String toString() {
+        String name = month.getDisplayName(TextStyle.FULL, Locale.getDefault());//cojo el nombre
+
+        name = Character.toUpperCase(name.charAt(0)) + name.substring(1, name.length());//primera letra mayuscula
+        //name = name.toUpperCase();//all mayuscula
+        //name = name.toLowerCase();//all minuscula, no hace falta, por defecto all in minusculas
+
+        name = month.getValue() + " - " + name;
+        return name;
+    }
+}
