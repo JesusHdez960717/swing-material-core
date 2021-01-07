711e4c4a679388933837687d8b86cbedfc8b2968
Unknown <unknown@example.com>
2021-01-07 16:36:55 -0500

    Licence a components

--- src/main/java/com/root101/swing/material/components/taskpane/CollapseMenu.java
+++ src/main/java/com/root101/swing/material/components/taskpane/CollapseMenu.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.root101.swing.material.components.taskpane;
 
@@ -11,7 +22,6 @@ import com.root101.swing.material.components.button.MaterialButtonIcon;
 import com.root101.swing.material.components.button.MaterialButtonsFactory;
 import com.root101.swing.material.components.labels.MaterialLabel;
 import com.root101.swing.material.components.labels.MaterialLabelsFactory;
-import com.root101.swing.material.components.labels._MaterialLabel;
 import com.root101.swing.material.effects.RippleEffect;
 import com.root101.swing.material.standards.MaterialColors;
 import com.root101.swing.ui.componentsui.panel.MaterialPanelUI;
@@ -35,10 +45,10 @@ import org.jdesktop.swingx.VerticalLayout;
 
 /**
  *
- * @author Jorge
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ * @author jjhurtado@Github
  */
-//TODO: fondo personalizable
-//TODO: 
 public class CollapseMenu extends JPanel {
 
     private final Icon iconoCategoria;
--- src/main/java/com/root101/swing/material/components/textfield/_MaterialTextField.java
+++ src/main/java/com/root101/swing/material/components/textfield/_MaterialTextField.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.swing.material.components.textfield;
 
 import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyEvent;
-import javax.swing.*;
 import com.root101.swing.util.MaterialDrawingUtils;
 import com.root101.swing.material.effects.DefaultFloatingLabel;
 import com.root101.swing.material.effects.FloatingLabel;
@@ -26,6 +41,9 @@ import com.root101.utils.services.ConverterService;
  * @see <a
  * href="https://www.google.com/design/spec/components/text-fields.html">Text
  * fields (Google design guidelines)</a>
+ * 
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class _MaterialTextField<T> extends MaterialTextField<T> {
 
