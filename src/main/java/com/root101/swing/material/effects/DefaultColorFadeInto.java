5e91f45c4b7719893677513f385d275fe468b79f
Unknown <unknown@example.com>
2021-01-07 16:41:05 -0500

    Licence al resto, efects & injection

--- src/main/java/com/root101/swing/material/effects/DefaultColorFadeInto.java
+++ src/main/java/com/root101/swing/material/effects/DefaultColorFadeInto.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.swing.material.effects;
 
 import com.root101.swing.util.SafePropertySetter;
@@ -6,10 +22,7 @@ import com.root101.swing.util.Utils;
 import java.awt.Color;
 import com.jhw.module.util.personalization.core.domain.Personalization;
 import com.jhw.module.util.personalization.services.PersonalizationHandler;
-import static com.root101.swing.util.DefaultMouseAdapterInfo.*;
 import com.root101.swing.util.MouseAdapterInfo;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.Serializable;
@@ -21,8 +34,8 @@ import org.jdesktop.core.animation.timing.interpolators.SplineInterpolator;
 /**
  * A floating label of a text field.
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
- * @param <T>
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class DefaultColorFadeInto<T extends JComponent & MouseAdapterInfo> implements ColorFadeInto, PropertyChangeListener, Serializable {
 
