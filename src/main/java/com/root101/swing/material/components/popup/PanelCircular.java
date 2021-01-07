9af1824e6e1b79b7785b386bb95a70786ffefbd9
Unknown <unknown@example.com>
2021-01-07 16:28:26 -0500

    Licence hasta splashScreen

--- src/main/java/com/root101/swing/material/components/popup/PanelCircular.java
+++ src/main/java/com/root101/swing/material/components/popup/PanelCircular.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.root101.swing.material.components.popup;
 
@@ -12,7 +23,6 @@ import com.root101.swing.util.MaterialDrawingUtils;
 import com.root101.utils.others.Pair;
 import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -29,7 +39,8 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class PanelCircular extends JPanel {
 
